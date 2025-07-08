797892ea063752962e1118fb89d3b5de93cd65c8
shafarizkyf <shafarizkyf@gmail.com>
2025-07-08 16:41:57 +0700

    normalize tokopedia store product list

--- tokopedia/store_list_products.js
+++ tokopedia/store_list_products.js
@@ -1,3 +1,20 @@
+class Normalize {
+  static price(valueStr) {
+    return Number(valueStr.replace('Rp', '').replace(/\./g, ''));
+  }
+
+  static discountPercentage(valueStr) {
+    return Number(valueStr.replace('%', ''))
+  }
+
+  static soldCounter(valueStr) {
+    return Number(valueStr.replace('terjual', '').trim())
+  }
+}
+
+const urlPaths = location.pathname.split('/');
+const storeName = urlPaths[1];
+
 const products = [];
 const productCardsEl = '.css-tjjb18 > .css-79elbk';
 document.querySelectorAll(productCardsEl).forEach((el) => {
@@ -16,27 +33,48 @@ document.querySelectorAll(productCardsEl).forEach((el) => {
   const normalPrice = productPriceEl.querySelector('div:nth-child(2) > span').textContent;
   const discountPercentage = productImageEl.querySelector('div > div:nth-child(2) > span:nth-child(1)').textContent.replace('>', '');
 
+  const ratingParentEl = el.querySelector('img[alt="rating"]')
+    ?.parentElement
+    ?.parentElement;
+
+  // look for "x terjual"
+  let soldCount = 0;
+  const elements = el.querySelectorAll('div > span');
+  const terjualElement = Array.from(elements).find(el =>
+    el.textContent.trim().match(/^\d+\s*terjual$/i)
+  );
+
+  if (terjualElement) {
+    soldCount = Normalize.soldCounter(terjualElement.textContent);
+  }
+
   const productCardData = {
     url: productLink,
     imageUrl: productImageUrl,
     name: productName,
-    discountPrice,
-    normalPrice,
-    discountPercentage
+    discountPrice: Normalize.price(discountPrice),
+    normalPrice: Normalize.price(normalPrice),
+    discountPercentage: Normalize.discountPercentage(discountPercentage),
+    ratingAvg: ratingParentEl ? Number(ratingParentEl.querySelector('span').textContent) : 0,
+    soldCount: soldCount,
   }
 
   products.push(productCardData);
 });
 
-console.log(products);
+const data = {
+  origin: location.href,
+  storeName: storeName,
+  data: products
+}
 
-const jsonStr = JSON.stringify(products, null, 2); // Pretty-printed
+const jsonStr = JSON.stringify(data, null, 2); // Pretty-printed
 const blob = new Blob([jsonStr], { type: "application/json" });
 const url = URL.createObjectURL(blob);
 
 const link = document.createElement("a");
 link.href = url;
-link.download = "products.json";
+link.download = `${storeName}.json`;
 document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);
