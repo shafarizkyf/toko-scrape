c5ca73687c48f7ae39383a6f4bb0e00bfccdc05e
shafarizkyf <shafarizkyf@gmail.com>
2025-07-08 20:08:28 +0700

    add shopee product scrape information and normalize number

--- shopee/store_list_products.js
+++ shopee/store_list_products.js
@@ -1,3 +1,27 @@
+class Normalize {
+  static price(valueStr) {
+    return Number(valueStr.replace(/\./g, ''));
+  }
+
+  static discountPercentage(valueStr) {
+    return Math.abs(Number(valueStr.replace('%', '')))
+  }
+
+  static soldCounter(valueStr) {
+    const numberWithUnit = valueStr
+      .replace('terjual', '')
+      .replace('+', '')
+      .trim();
+
+    if (numberWithUnit.includes('RB')) {
+      const amountStr = numberWithUnit.replace('RB', '');
+      return Number(amountStr) * 1000;
+    }
+
+    return Number(numberWithUnit);
+  }
+}
+
 const storeName = location.pathname.replace('/', '');
 
 const products = [];
@@ -11,20 +35,42 @@ document.querySelectorAll('.shop-search-result-view > .row > div').forEach(el =>
   const price = cardInfoEl.querySelector('div:nth-child(2) div.items-baseline > span:nth-child(2)').textContent;
   const discountPercentage = cardInfoEl.querySelector('div:nth-child(2) > div:nth-child(2) > span')?.getAttribute('aria-label') || null;
 
+  const ratingEl = el.querySelector('img[alt="rating-star-full"]')?.parentElement;
+  const ratingAvg = ratingEl ? ratingEl.querySelector('div').textContent : 0;
+
+  // look for "x terjual"
+  let soldCount = 0;
+  const elements = el.querySelectorAll('div > div.text-shopee-black87');
+  const soldEl = Array.from(elements).find(el =>
+    el.textContent.trim().match(/terjual$/i)
+  );
+
+  if (soldEl) {
+    soldCount = Normalize.soldCounter(soldEl.textContent);
+  }
+
   const product = {
     url: productLink,
     imageUrl: productImageUrl,
     name: title,
-    price,
-    discountPercentage
+    price: Normalize.price(price),
+    discountPercentage: discountPercentage ? Normalize.discountPercentage(discountPercentage) : null,
+    ratingAvg: Number(ratingAvg),
+    soldCount,
   };
-
-  console.table(product);
 
   products.push(product);
 });
 
-const jsonStr = JSON.stringify(products, null, 2); // Pretty-printed
+const data = {
+  origin: location.href,
+  storeName: storeName,
+  data: products
+}
+
+console.log({ products });
+
+const jsonStr = JSON.stringify(data, null, 2); // Pretty-printed
 const blob = new Blob([jsonStr], { type: "application/json" });
 const myurl = URL.createObjectURL(blob);
 
